279e98a34095a5b680a64a4ff9b462a7d91e501a
wangjie36 <wangjie36@gome.com.cn>
2018-07-05 18:21:11 +0800

    CA到PA，改成异步http通信

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/HelloController.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/HelloController.java
@@ -5,21 +5,26 @@ import com.alibaba.dubbo.performance.demo.agent.registry.Endpoint;
 import com.alibaba.dubbo.performance.demo.agent.registry.EtcdRegistry;
 import com.alibaba.dubbo.performance.demo.agent.registry.IRegistry;
 import okhttp3.*;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.ListenableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class HelloController {
 
     private Logger logger = LoggerFactory.getLogger(HelloController.class);
-    
+
     private IRegistry registry = new EtcdRegistry(System.getProperty("etcd.url"));
 
     private RpcClient rpcClient = new RpcClient(registry);
@@ -27,6 +32,8 @@ public class HelloController {
     private List<Endpoint> endpoints = null;
     private Object lock = new Object();
     private OkHttpClient httpClient = new OkHttpClient();
+
+    private AsyncHttpClient asyncHttpClient = org.asynchttpclient.Dsl.asyncHttpClient();
 
 
     @RequestMapping(value = "")
@@ -66,23 +73,54 @@ public class HelloController {
 
         String url =  "http://" + endpoint.getHost() + ":" + endpoint.getPort();
 
-        RequestBody requestBody = new FormBody.Builder()
-                .add("interface",interfaceName)
-                .add("method",method)
-                .add("parameterTypesString",parameterTypesString)
-                .add("parameter",parameter)
+        // 同步请求
+//        RequestBody requestBody = new FormBody.Builder()
+//                .add("interface",interfaceName)
+//                .add("method",method)
+//                .add("parameterTypesString",parameterTypesString)
+//                .add("parameter",parameter)
+//                .build();
+//
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .post(requestBody)
+//                .build();
+//
+//        try (okhttp3.Response response = httpClient.newCall(request).execute()) {
+//            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+//            byte[] bytes = response.body().bytes();
+//            String s = new String(bytes).trim();
+//            return Integer.valueOf(s);
+//        }
+
+
+
+        // 异步请求
+        DeferredResult<Integer> result = new DeferredResult<>();
+
+        org.asynchttpclient.Request request = org.asynchttpclient.Dsl.post(url)
+                .addFormParam("interface", interfaceName)
+                .addFormParam("method", method)
+                .addFormParam("parameterTypesString", parameterTypesString)
+                .addFormParam("parameter", parameter)
                 .build();
 
-        Request request = new Request.Builder()
-                .url(url)
-                .post(requestBody)
-                .build();
+        ListenableFuture<org.asynchttpclient.Response> responseFuture = asyncHttpClient.executeRequest(request);
 
-        try (Response response = httpClient.newCall(request).execute()) {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-            byte[] bytes = response.body().bytes();
-            String s = new String(bytes).trim();
-            return Integer.valueOf(s);
+        Runnable callback = () -> {
+            try {
+                String value = responseFuture.get().getResponseBody();
+                result.setResult(Integer.valueOf(value.trim()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        responseFuture.addListener(callback, null);
+
+        while (!responseFuture.isDone()) {
+            TimeUnit.MILLISECONDS.sleep(1);
         }
+
+        return (Integer) result.getResult();
     }
 }
